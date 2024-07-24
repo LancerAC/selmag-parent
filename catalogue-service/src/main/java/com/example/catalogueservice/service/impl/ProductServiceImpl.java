adb5a53f55af411ac811d9f15253b70ad2530d0f
LancerAC <ilya.pashchenko.04@inbox.ru>
2024-07-24 20:09:11 +0300

    add Transactional annotation

--- catalogue-service/src/main/java/com/example/catalogueservice/service/impl/ProductServiceImpl.java
+++ catalogue-service/src/main/java/com/example/catalogueservice/service/impl/ProductServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.catalogueservice.service.impl;
 import com.example.catalogueservice.entity.Product;
 import com.example.catalogueservice.repository.ProductRepository;
 import com.example.catalogueservice.service.ProductService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,21 +18,27 @@ public class ProductServiceImpl implements ProductService {
     private final ProductRepository productRepository;
 
     @Override
-    public List<Product> findAllProducts() {
-        return this.productRepository.findAll();
+    public Iterable<Product> findAllProducts(String filter) {
+        if (filter != null && !filter.isBlank()) {
+            return this.productRepository.findAllByTitleLikeIgnoreCase("%" + filter + "%");
+        } else {
+            return this.productRepository.findAll();
+        }
     }
 
     @Override
+    @Transactional
     public Product createProduct(String title, String details) {
         return this.productRepository.save(new Product(null, title, details));
     }
 
     @Override
-    public Optional<Product> findProductById(int productId) {
+    public Optional<Product> findProduct(int productId) {
         return this.productRepository.findById(productId);
     }
 
     @Override
+    @Transactional
     public void updateProduct(Integer id, String title, String details) {
         this.productRepository.findById(id)
                 .ifPresentOrElse(product -> {
@@ -43,6 +50,7 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
+    @Transactional
     public void deleteProduct(Integer id) {
         this.productRepository.deleteById(id);
     }
