db7f20154b3731d2c9b3f1fcb33bf2bf02a864a8
LancerAC <ilya.pashchenko.04@inbox.ru>
2024-07-25 17:02:40 +0300

    add http status param in create product method

--- manager-app/src/main/java/com/example/managerapp/web/controller/ProductsController.java
+++ manager-app/src/main/java/com/example/managerapp/web/controller/ProductsController.java
@@ -4,7 +4,9 @@ import com.example.managerapp.client.BadRequestException;
 import com.example.managerapp.client.ProductsRestClient;
 import com.example.managerapp.entity.Product;
 import com.example.managerapp.web.payload.NewProductPayload;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -31,11 +33,13 @@ public class ProductsController {
 
     @PostMapping("create")
     public String createProduct(NewProductPayload payload,
-                                Model model) {
+                                Model model,
+                                HttpServletResponse response) {
         try {
             Product product = this.productsRestClient.createProduct(payload.title(), payload.details());
             return "redirect:/catalogue/products/%d".formatted(product.id());
         } catch (BadRequestException exception) {
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
             model.addAttribute("payload", payload);
             model.addAttribute("errors", exception.getErrors());
             return "catalogue/products/new_product";
