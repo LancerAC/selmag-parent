9899190ffe17668ec8d26cf87a57a16216d0bb82
LancerAC <ilya.pashchenko.04@inbox.ru>
2024-07-26 13:43:35 +0300

    create entity for mongodb

---
+++ feedback-service/src/main/java/com/example/feedbackservice/entity/ProductReview.java
@@ -0,0 +1,25 @@
+package com.example.feedbackservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document("product_review")
+public class ProductReview {
+
+    @Id
+    private UUID id;
+
+    private int productId;
+
+    private int rating;
+
+    private String review;
+}
