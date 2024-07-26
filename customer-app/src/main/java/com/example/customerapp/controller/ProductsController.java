cf11491fdb2ee8600ed7ecab30c156a2c364a1f5
LancerAC <ilya.pashchenko.04@inbox.ru>
2024-07-26 13:44:28 +0300

    add method for get favourites products

--- customer-app/src/main/java/com/example/customerapp/controller/ProductsController.java
+++ customer-app/src/main/java/com/example/customerapp/controller/ProductsController.java
@@ -1,9 +1,10 @@
 package com.example.customerapp.controller;
 
+import com.example.customerapp.client.FavouriteProductsClient;
 import com.example.customerapp.client.ProductsClient;
-import org.springframework.stereotype.Controller;
-
+import com.example.customerapp.entity.FavouriteProduct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,15 +16,31 @@ import reactor.core.publisher.Mono;
 @RequestMapping("customer/products")
 public class ProductsController {
 
-    private ProductsClient productsClient;
+    private final ProductsClient productsClient;
 
-    @GetMapping
-    public Mono<String> getProductsListPage(
-            Model model,
-            @RequestParam(name = "filter", required = false) String filter){
+    private final FavouriteProductsClient favouriteProductsClient;
+
+    @GetMapping("list")
+    public Mono<String> getProductsListPage(Model model,
+                                            @RequestParam(name = "filter", required = false) String filter) {
+        model.addAttribute("filter", filter);
         return this.productsClient.findAllProducts(filter)
                 .collectList()
                 .doOnNext(products -> model.addAttribute("products", products))
                 .thenReturn("customer/products/list");
+    }
+
+    @GetMapping("favourites")
+    public Mono<String> getFavouriteProductsPage(Model model,
+                                                 @RequestParam(name = "filter", required = false) String filter) {
+        model.addAttribute("filter", filter);
+        return this.favouriteProductsClient.findFavouriteProducts()
+                .map(FavouriteProduct::productId)
+                .collectList()
+                .flatMap(favouriteProducts -> this.productsClient.findAllProducts(filter)
+                        .filter(product -> favouriteProducts.contains(product.id()))
+                        .collectList()
+                        .doOnNext(products -> model.addAttribute("products", products)))
+                .thenReturn("customer/products/favourites");
     }
 }
