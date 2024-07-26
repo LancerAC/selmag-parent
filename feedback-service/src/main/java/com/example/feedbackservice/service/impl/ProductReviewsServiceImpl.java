e624849deb0b66128f4e726ae63b03ba49b3a6f4
LancerAC <ilya.pashchenko.04@inbox.ru>
2024-07-26 13:42:36 +0300

    create service for product reviews and implementation

---
+++ feedback-service/src/main/java/com/example/feedbackservice/service/impl/ProductReviewsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.feedbackservice.service.impl;
+
+import com.example.feedbackservice.entity.ProductReview;
+import com.example.feedbackservice.repository.ProductReviewRepository;
+import com.example.feedbackservice.service.ProductReviewsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class ProductReviewsServiceImpl implements ProductReviewsService {
+
+    private final ProductReviewRepository productReviewRepository;
+
+    @Override
+    public Mono<ProductReview> createProductReview(int productId, int rating, String review) {
+        return this.productReviewRepository.save(
+                new ProductReview(UUID.randomUUID(), productId, rating, review));
+    }
+
+    @Override
+    public Flux<ProductReview> findProductReviewsByProduct(int productId) {
+        return this.productReviewRepository.findAllByProductId(productId);
+    }
+}
