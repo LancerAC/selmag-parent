1ced83c38999cabac7556e2341a31a9657f29fdf
LancerAC <ilya.pashchenko.04@inbox.ru>
2024-07-26 13:45:44 +0300

    add method for find product by id

--- customer-app/src/main/java/com/example/customerapp/client/ProductsClient.java
+++ customer-app/src/main/java/com/example/customerapp/client/ProductsClient.java
@@ -2,8 +2,11 @@ package com.example.customerapp.client;
 
 import com.example.customerapp.entity.Product;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface ProductsClient {
 
     Flux<Product> findAllProducts(String filter);
+
+    Mono<Product> findProduct(int id);
 }
