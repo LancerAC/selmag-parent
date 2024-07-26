4dd6180ee8cde5b6e3ec111c664555a91b431ee2
LancerAC <ilya.pashchenko.04@inbox.ru>
2024-07-26 12:08:42 +0300

    create payload with validation

--- customer-app/src/main/java/com/example/customerapp/controller/payload/NewProductReviewPayload.java
+++ customer-app/src/main/java/com/example/customerapp/controller/payload/NewProductReviewPayload.java
@@ -1,4 +1,15 @@
 package com.example.customerapp.controller.payload;
 
-public record NewProductReviewPayload(Integer rating, String review) {
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record NewProductReviewPayload(
+        @NotNull(message = "{customer.products.reviews.create.errors.rating_is_null}")
+        @Min(value = 1, message = "{customer.products.reviews.create.errors.rating_is_below_min}")
+        @Max(value = 5, message = "{customer.products.reviews.create.errors.rating_is_above_max}")
+        Integer rating,
+        @Size(max = 1000, message = "{customer.products.reviews.create.errors.review_is_too_big}")
+        String review) {
 }
