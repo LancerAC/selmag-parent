c0fe2487cdb927db79d86a775222e6e1e51ebe05
LancerAC <ilya.pashchenko.04@inbox.ru>
2024-07-27 12:00:49 +0300

    add arg userId

--- feedback-service/src/main/java/com/example/feedbackservice/service/impl/ProductReviewsServiceImpl.java
+++ feedback-service/src/main/java/com/example/feedbackservice/service/impl/ProductReviewsServiceImpl.java
@@ -17,9 +17,9 @@ public class ProductReviewsServiceImpl implements ProductReviewsService {
     private final ProductReviewRepository productReviewRepository;
 
     @Override
-    public Mono<ProductReview> createProductReview(int productId, int rating, String review) {
+    public Mono<ProductReview> createProductReview(int productId, int rating, String review, String userId) {
         return this.productReviewRepository.save(
-                new ProductReview(UUID.randomUUID(), productId, rating, review));
+                new ProductReview(UUID.randomUUID(), productId, rating, review, userId));
     }
 
     @Override
