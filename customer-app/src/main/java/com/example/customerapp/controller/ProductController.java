7b4dc10223745d925b096efc563ab238a9e66872
LancerAC <ilya.pashchenko.04@inbox.ru>
2024-07-27 11:56:05 +0300

    add method for load CSRF token

--- customer-app/src/main/java/com/example/customerapp/controller/ProductController.java
+++ customer-app/src/main/java/com/example/customerapp/controller/ProductController.java
@@ -4,13 +4,16 @@ import com.example.customerapp.client.FavouriteProductsClient;
 import com.example.customerapp.client.ProductReviewsClient;
 import com.example.customerapp.client.ProductsClient;
 import com.example.customerapp.client.exception.ClientBadRequestException;
-import com.example.customerapp.client.payload.NewProductReviewPayload;
+import com.example.customerapp.controller.payload.NewProductReviewPayload;
 import com.example.customerapp.entity.Product;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.web.reactive.result.view.CsrfRequestDataValueProcessor;
+import org.springframework.security.web.server.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.util.NoSuchElementException;
@@ -84,5 +87,12 @@ public class ProductController {
     public String handleNoSuchElementException(NoSuchElementException exception, Model model) {
         model.addAttribute("error", exception.getMessage());
         return "errors/404";
+    }
+
+    @ModelAttribute
+    public Mono<CsrfToken> loadCsrfToken(ServerWebExchange exchange) {
+        return exchange.<Mono<CsrfToken>>getAttribute(CsrfToken.class.getName())
+                .doOnSuccess(token -> exchange.getAttributes()
+                        .put(CsrfRequestDataValueProcessor.DEFAULT_CSRF_ATTR_NAME, token));
     }
 }
