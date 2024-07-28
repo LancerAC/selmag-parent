8f93655e873bf1c1f63978c6ea440cf892813705
LancerAC <ilya.pashchenko.04@inbox.ru>
2024-07-28 17:56:49 +0300

    create OauthHttpHeadersProvider

---
+++ admin-server/src/main/java/com/example/adminserver/web/client/OauthHttpHeadersProvider.java
@@ -0,0 +1,25 @@
+package com.example.adminserver.web.client;
+
+import de.codecentric.boot.admin.server.domain.entities.Instance;
+import de.codecentric.boot.admin.server.web.client.HttpHeadersProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+
+@RequiredArgsConstructor
+public class OauthHttpHeadersProvider implements HttpHeadersProvider {
+
+    private final OAuth2AuthorizedClientManager clientManager;
+
+    @Override
+    public HttpHeaders getHeaders(Instance instance) {
+        OAuth2AuthorizedClient keycloak = clientManager.authorize(OAuth2AuthorizeRequest.withClientRegistrationId("keycloak")
+                .principal("admin-service")
+                .build());
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(keycloak.getAccessToken().getTokenValue());
+        return headers;
+    }
+}
